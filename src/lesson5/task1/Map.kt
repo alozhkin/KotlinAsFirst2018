5e6e02c3f55e21c206322e3a72c3f3b1de94c636
alozhkin <alozhkin99@gmail.com>
2018-11-07 00:17:04 +0300

    Сделал рукопожатия (возможно)

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -202,78 +202,15 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
 // Во FRIENDS хранится значение агрумента propagateHandshakes
 // В BUFFER заносятся промежуточные и конечные значения для каждого человека
 // NAMESTACK -- хранит список всех имён, вызванных до этого
-var FRIENDS = mapOf<String, Set<String>>()
-var BUFFER = mutableMapOf<String, MutableSet<String>>()
-var NAMESTACK = mutableListOf<String>()
-var WAS = mutableMapOf<String, Int>()
 
-
-fun prpg (str: String) {
-    if (FRIENDS[str] != null) {
-        // вызываю функцию, пока не дойду до родительской вершины, имени, отсутствующего в FRIENDS,
-        // или пустого множества
-        if (!NAMESTACK.contains(str)) {
-            // добавляю имя, для которого вызывалась функция, ко всем значениям имён в стеке
-            for (name in NAMESTACK) {
-                if (BUFFER[name] == null) {
-                    BUFFER[name] = mutableSetOf()
-                }
-                BUFFER[name]!!.add(str)
-            }
-            if (FRIENDS[str]!!.isEmpty()) {
-                // создаю для имён, имеющих пустое множество в качестве значения, запись в буфере
-                BUFFER[str] = mutableSetOf()
-            } else {
-                // добавляю имя в стек и перехожу к дочерним вершинам
-                NAMESTACK.add(str)
-                for (element in FRIENDS[str]!!) {
-                    prpg(element)
-                }
-                NAMESTACK.remove(NAMESTACK.last())
-            }
-        }
-    } else {
-        // создаю для имён, отсутствующих в FRIENDS, запись в буфере и добавляю их в значения всех имён в стеке
-        for (name in NAMESTACK) {
-            if (BUFFER[name] == null) {
-                BUFFER[name] = mutableSetOf()
-            }
-            BUFFER[name]!!.add(str)
-        }
-        BUFFER[str] = mutableSetOf()
-    }
-}
-// родительские вершины могут вызываться из дочерних, ещё до того, как функция прошла все вершины, необходимые,
-// чтобы получить их полные значения. Чтобы этого избежать необходимо во второй раз пройти по графу и отдать
-// дочерним вершинам уже полученные полные значения родительских вершин и их имена.
-fun prpg2 (str: String) {
-    if (FRIENDS[str] != null && BUFFER[str] != null) {
-        if (!NAMESTACK.contains(str)) {
-            // вызываю функцию для всех дочерних вершин, пока не наткнусь на родительскую или пустое множество
-            // очень важно, чтобы в случае развлетвления функция сначала вызывалась для родительских вершин,
-            // чтобы они могли отдать значения
-            NAMESTACK.add(str)
-            for (element in FRIENDS[str]!!.sortedBy { !NAMESTACK.contains(it) }) {
-                prpg2(element)
-            }
-            NAMESTACK.remove(NAMESTACK.last())
-        } else {
-            // здесь программа отдаёт значение и имя верхней вершины всем именам в стеке
-            for (name in NAMESTACK) {
-                if (name != str) {
-                    // name вычитается, потому что нельзя быть знакомым с самим собой
-                    BUFFER[name]!!.addAll(BUFFER[str]!! + str)
-                    BUFFER[name]!!.remove(name)
-                }
-            }
-        }
-    }
-}
 
 fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<String>> {
 
+
     var nameStack = mutableListOf<String>()
     var buffer = mutableMapOf<String, MutableSet<String>>()
+    var alreadyWas = mutableMapOf<String, Int>()
+
 
     fun foo(name: String) {
         // вызываю функцию, пока не дойду до вершины, которая уже была
@@ -326,12 +263,33 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
             }
         }
     }
+
+    fun foo3(str: String) {
+        if (friends[str] != null && buffer[str] != null) {
+            nameStack.add(str)
+            for (elem in friends[str]!!.sortedBy { !nameStack.contains(it) }) {
+                for (name in nameStack) {
+                    buffer[name]!!.addAll(buffer[elem]!! + elem - name)
+                }
+                if (alreadyWas[elem] != 1) {
+                    alreadyWas[elem] = 1
+                    foo3(elem)
+                }
+            }
+            nameStack.remove(nameStack.last())
+        }
+    }
+
+    buffer.keys.forEach { key ->
+        alreadyWas[key] = 0
+    }
     friends.keys.forEach { key ->
         if (!buffer.containsKey(key)) {
             foo(key)
-            foo2(key)
+            foo3(key)
         }
     }
+
     return buffer
 }
 
